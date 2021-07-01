80a4095d20c18075f527cf2fdef6435477f562da
Kirill <berdinskikh.pro@gmail.com>
2021-07-02 00:22:18 +0500

    Сделал мобильную версию шапки

---
+++ source/js/app.js
@@ -0,0 +1,15 @@
+let navToggle = document.querySelector('.main-nav__toggle');
+let siteList = document.querySelector('.site-list');
+let navMain = document.querySelector('.main-nav');
+
+navMain.classList.remove('main-nav-nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  }
+})
